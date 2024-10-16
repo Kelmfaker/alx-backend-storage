8cced2fe6d95b421eaa1e3890369fb2782741e9e
Kelmfaker <kelmfaker@gmail.com>
2024-10-16 23:43:00 +0000

    update file

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,6 @@
+-- This SQL file creates a new table for storing user information
+-- It includes columns for user ID, name, email, and registration date
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
