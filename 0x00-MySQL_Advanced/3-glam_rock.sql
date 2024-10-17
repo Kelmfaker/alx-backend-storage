27c16d8f12e88415919cc95271fe3bce28b4d5f5
Kelmfaker <kelmfaker@gmail.com>
2024-10-17 21:08:15 +0000

    glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,7 @@
 -- This file lists all bands with Glam rock
-SELECT band_name, 
-       (2024 - formed) - (CASE WHEN split IS NOT NULL THEN (2024 - split) ELSE (2024 - formed) END) AS lifespan 
+SELECT band_name,
+       IFNULL(split, 2022) - formed AS lifespan
 FROM metal_bands 
-WHERE style = 'Glam rock';
+WHERE style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
