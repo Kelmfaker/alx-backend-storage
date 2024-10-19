a76f4ad1fe930583bb6cf37d67cbd64f528e11f8
Kelmfaker <kelmfaker@gmail.com>
2024-10-19 23:54:51 +0000

    valid email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- a trigger that resets the attribute valid_email
+DELIMITER //
+
+CREATE TRIGGER valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+
+//
+
+DELIMITER ;
