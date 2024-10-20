a3ee0536ee5737eace1527e31668d8fa0146e8dc
Kelmfaker <kelmfaker@gmail.com>
2024-10-20 00:36:35 +0000

    average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,34 @@
+-- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
+    DECLARE average_score FLOAT DEFAULT 0;
+
+    -- Calculate the total score and the number of projects
+    SELECT SUM(score), COUNT(*)
+    INTO total_score, projects_count
+    FROM corrections
+    WHERE user_id = user_id;
+
+    -- Calculate the average score
+    IF projects_count > 0 THEN
+        SET average_score = total_score / projects_count;
+    ELSE
+        SET average_score = 0;
+    END IF;
+
+    -- Update the user's average score
+    UPDATE users
+    SET average_score = average_score
+    WHERE id = user_id;
+END;
+
+//
+
+DELIMITER ;
