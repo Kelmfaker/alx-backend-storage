c9e10426d86951cdd3545c8b94df3f473421c565
Kelmfaker <kelmfaker@gmail.com>
2024-10-23 20:10:45 +0000

    recovering

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,7 @@ Writing strings to Redis
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -19,3 +19,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None)
+    -> Union[str, bytes, int, float, None]:
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, int)
